5649904e63b06764d004cb0fc5c499c4acffbef0
Pratik Mahankal <pratik.mahankal14@gmail.com>
2021-02-27 21:47:03 +0530

    Added code Sign of hand

--- Capturing-Images/Sign-Rec-Dataset-Generator/capture.py
+++ Capturing-Images/Sign-Rec-Dataset-Generator/capture.py
@@ -1,4 +1,7 @@
 '''
+Dataset Creator for Sign Gesture Recognition
+'''
+'''
 Input - 
     if q is pressed - Terminate the program.
     if c is pressed - Capture the image 
@@ -7,32 +10,29 @@ Input -
     the reflection of white light might be problematic
 '''
 '''
-//TODO: Apply appropriate cv on the image
-	# Find cv
-	# Get ROI
-	# Uderstand accumulated_weight
-	# Apply it for the background
 
-//TODO: Understand clicking a  normal image
-//TODO: Click multiple images
-//TODO: Store in folder with correct naming
 //TODO: Make a thumbs up and thumbs down dataset
 
 '''
 
-
-import cv2
 import os
+import cv2
 import numpy as np
 
 #Fix the directory
-# directory = r'D:\Computer Vision\CV\bin-test\n'
-# os.chdir(directory) 
+directory = r'D:\Computer Vision\OpenCV-projects\Capturing-Images\Sign-Rec-Dataset-Generator'
+os.chdir(directory) 
 
 #Input from the web camera
 cap = cv2.VideoCapture(0)
 
+#Variable used while writing the image.
 i=0 
+
+# These lower and upper bounds are there to calculate the hsv color of the hand.
+l_b=np.array((0,56,0)) 
+u_b=np.array((255,255,255))
+
 while(True):
     ret, frame = cap.read()
 
@@ -44,27 +44,32 @@ while(True):
     if key==ord('q'):
         break
 
-    # #If 'c' pressed then capture.
-    # if key==ord('c'):
-    #     cv2.imshow("imshow2",frame)
+    #If 'c' pressed then capture.
+    if key==ord('c'):
 
-    #     i+=1
+        #BGR to HSV
+        hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-    #     #Converting BGR to HSV
-    #     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        
-    #     #Here we have to set the lower and upper bounds of HSV
-    #     l_b=np.array((102,102,0))
-    #     u_b=np.array((160,255,255))
+        #Masking the image and applying bitwise_and operator.
+        mask=cv2.inRange(hsv,l_b,u_b)
+        frame=cv2.bitwise_and(frame,frame,mask=mask)
 
-    #     #Masking the HSV image
-    #     mask=cv2.inRange(hsv,l_b,u_b)
-    #     res=cv2.bitwise_and(frame,frame,mask=mask)
-    #     cv2.imshow('res',res)
+        #Gray Scaling the image.
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #     #Save the hsv image with the file name.
-    #     filename = str(i)+'.png'
-    #     cv2.imwrite(filename, res)
+        #Applying Gaussian Blur with kernel of 9*9
+        frame = cv2.GaussianBlur(frame, (9, 9), 0)
+
+        #Applying Thresholding.
+        ret,frame = cv2.threshold(frame,25,255,cv2.THRESH_BINARY)
+
+        cv2.imshow("imshow2",frame)
+
+        #Save the hsv image with the file name.
+        i+=1
+        filename = str(i)+'.png'
+        print(filename)
+        # cv2.imwrite(filename, frame)
 
 # release the capture
 cap.release()
